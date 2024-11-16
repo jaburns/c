30d011223f3776caae033667bec51ad9c595cabf
jaburns <j@jaburns.net>
2024-11-16 16:17:55 -0700

    Use timing module in test module

--- base/test.c
+++ base/test.c
@@ -1,25 +1,11 @@
 #include "inc.h"
 
-global u64 g_timer_start_secs;
-
-internal void test_timer_init(void) {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    timer_start_secs = ts.tv_sec;
-}
-
-internal u64 test_timer_get_nanos(void) {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return 1000000000l * (ts.tv_sec - timer_start_secs) + ts.tv_nsec;
-}
-
 internal void test_run_(char* name, void (*fn)(void)) {
-    test_timer_init();
+    timing_global_init();
     fprintf(stderr, "> Running: %s", name);
-    u64 start_nanos = test_timer_get_nanos();
+    u64 start_nanos = timing_get_nanos_since_start();
     fn();
-    u64 finish_nanos = test_timer_get_nanos();
+    u64 finish_nanos = timing_get_nanos_since_start();
     fprintf(stderr, "\t- completed in %llu μs\n", (finish_nanos - start_nanos) / 1000);
 }
 
