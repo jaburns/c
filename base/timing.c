5f75ea68d9a5f6962e211fbffd31bca128e80c2f
jaburns <j@jaburns.net>
2024-12-04 15:30:36 -0700

    Fix timing interface

--- base/timing.c
+++ base/timing.c
@@ -1,7 +1,6 @@
 #include "inc.h"
 
 #ifdef __APPLE__
-#ifdef __aarch64__
 
 global mach_timebase_info_data_t g_timing_timebase;
 
@@ -10,32 +9,19 @@ internal void timing_global_init(void) {
 }
 
 internal u64 timing_get_ticks(void) {
+#if 0  // def __aarch64__
     u64 ret;
     __asm__ volatile("mrs %0, cntvct_el0" : "=r"(ret));
     return ret;
+#else
+    return mach_absolute_time();
+#endif
 }
 
 internal u64 timing_ticks_to_nanos(u64 ticks) {
     return ticks * g_timing_timebase.numer / g_timing_timebase.denom;
 }
 
-#else
-
-global mach_timebase_info_data_t g_timing_timebase;
-global u64                       g_timing_start_ticks;
-
-internal void timing_global_init(void) {
-    mach_timebase_info(&g_timing_timebase);
-    g_timing_start_ticks = mach_absolute_time();
-}
-
-internal u64 timing_get_nanos_since_start(void) {
-    u64 elapsed    = mach_absolute_time() - g_timing_start_ticks;
-    u64 elapsed_ns = elapsed * g_timing_timebase.numer / g_timing_timebase.denom;
-    return elapsed_ns;
-}
-
-#endif
 #else
 
 global u64 g_timing_start_secs;
@@ -46,10 +32,14 @@ internal void timing_global_init(void) {
     g_timing_start_secs = ts.tv_sec;
 }
 
-internal u64 timing_get_nanos_since_start(void) {
+internal u64 timing_get_ticks(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return 1000000000l * (ts.tv_sec - g_timing_start_secs) + ts.tv_nsec;
+}
+
+internal u64 timing_ticks_to_nanos(u64 ticks) {
+    return ticks;
 }
 
 #endif
--- base/timing.h
+++ base/timing.h
@@ -1,4 +1,5 @@
 #pragma once
 
 internal void timing_global_init(void);
-internal u64  timing_get_nanos_since_start(void);
+internal u64  timing_get_ticks(void);
+internal u64  timing_ticks_to_nanos(u64 ticks);
